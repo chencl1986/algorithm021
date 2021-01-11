c5c9b7b3221017720351dcfb9fe5c46a4b8cc1a7
陈立 <leechen@chenlideMacBook-Air.local>
2021-01-11 23:46:31 +0800

    feat(Week07): 提交作业

---
+++ Week_07/200.岛屿数量，并查集.js
@@ -0,0 +1,108 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=javascript
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (grid) {
+  const m = grid.length; // 缓存行数量
+  const n = grid[0].length; // 缓存列数量
+  const dx = [1, -1, 0, 0]; // 缓存行方向
+  const dy = [0, 0, 1, -1]; // 缓存列方向
+  // 根据grid上的陆地情况，初始化并查集
+  let unionFind = new UnionFind(grid, m, n);
+
+  // 遍历整个地图，将陆地都合并到一个集合中
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      // 如果是陆地，就进行集合的合并
+      if (grid[i][j] === '1') {
+        // 将当前位置设置为0，避免重复合并
+        grid[i][j] = '0';
+
+        // 计算当前坐标四周的的点坐标
+        for (let k = 0; k < 4; k++) {
+          // 计算新的i和j
+          const newI = i + dx[k];
+          const newJ = j + dy[k];
+
+          // 当新坐标在地图内，且也是陆地时，将这些点都合并到一个集合
+          if (
+            newI >= 0 &&
+            newJ >= 0 &&
+            newI < m &&
+            newJ < n &&
+            grid[newI][newJ] === '1'
+          ) {
+            // 将所有泸定合并到一个集合，并进行计数
+            unionFind.union(newI * n + newJ, i * n + j);
+          }
+        }
+      }
+    }
+  }
+
+  // 所有陆地都合并后，剩下的集合数量就是岛屿数量
+  return unionFind.count;
+};
+
+// 并查集
+class UnionFind {
+  constructor(grid, m, n) {
+    this.count = 0; // 统计集合数量，初始化为n
+    this.parent = new Array(m * n); // 使用数组保存集合
+
+    // 根据grid的值初始化所有集合
+    for (let i = 0; i < m; i++) {
+      for (let j = 0; j < n; j++) {
+        // 计算grid索引对应并查集中的索引
+        const index = i * n + j;
+
+        // 只有是陆地的位置才是集合元素
+        if (grid[i][j] === '1') {
+          this.parent[index] = index; // 对元素做标记
+          this.count++; // 对集合进行计数
+        } else {
+          this.parent[index] = -1; // 非集合元素标记为-1
+        }
+      }
+    }
+  }
+
+  // 查找一个节点所在集合的根节点
+  find(p) {
+    // 当前节点的父节点是其自身时，它就是集合的根节点
+    while (p != this.parent[p]) {
+      // 将当前节点父节点指针，直接指向爷爷节点，实现路径压缩
+      this.parent[p] = this.parent[this.parent[p]];
+      // 将指针向上移动
+      p = this.parent[p];
+    }
+
+    // 将找到的根节点返回
+    return p;
+  }
+
+  // 合并两个集合
+  union(p, q) {
+    // 找到两个节点所在集合的根节点
+    let rootP = this.find(p);
+    let rootQ = this.find(q);
+
+    // 如果他们是同一个节点，表示p和q在同一个集合，无需合并
+    if (rootP === rootQ) {
+      return;
+    }
+
+    // 将rootP的根节点指向rootQ，完成集合的合并
+    this.parent[rootP] = rootQ;
+    // 减少集合数量
+    this.count--;
+  }
+}
+// @lc code=end
---
+++ Week_07/208. 实现 Trie (前缀树).js
@@ -0,0 +1,95 @@
+/*
+ * @lc app=leetcode.cn id=208 lang=javascript
+ *
+ * [208] 实现 Trie (前缀树)
+ */
+
+// @lc code=start
+/**
+ * Initialize your data structure here.
+ */
+var Trie = function () {
+  // 根节点使用一个空对象，单词的每个字母作为对象的一个key
+  this.root = {};
+  // 使用一个固定的标记，标记一个节点为字典树的根节点
+  // 此处使用Symbol标记，避免重复
+  this.endOfWord = Symbol('EOF');
+};
+
+/**
+ * Inserts a word into the trie.
+ * @param {string} word
+ * @return {void}
+ */
+Trie.prototype.insert = function (word) {
+  // 当一个单词插入字典树时，都从根节点开始插入
+  let node = this.root;
+
+  // 逐个字母遍历单词
+  for (const char of word) {
+    // 如果字母未在字典树中川建国节点，则创建一个新节点
+    node[char] = node[char] || {};
+    // 移动节点，向下遍历
+    node = node[char];
+  }
+
+  // 标记当前节点是一个单词的结束位置
+  node[this.endOfWord] = this.endOfWord;
+};
+
+/**
+ * Returns if the word is in the trie.
+ * @param {string} word
+ * @return {boolean}
+ */
+Trie.prototype.search = function (word) {
+  // 单词的搜索从根节点开始
+  let node = this.root;
+
+  // 在字典树中查找单词的每个字母
+  for (const char of word) {
+    // 如果当前字母不存在于字典树，该单词也一定不存在
+    if (!node[char]) {
+      return false;
+    }
+
+    // 移动节点，向下查找
+    node = node[char];
+  }
+
+  // 如果该单词的最后一个字母对应的节点，在字典树中被标记为单词结尾，才表示这个单词在字典树中存在
+  return node[this.endOfWord] === this.endOfWord;
+};
+
+/**
+ * Returns if there is any word in the trie that starts with the given prefix.
+ * @param {string} prefix
+ * @return {boolean}
+ */
+Trie.prototype.startsWith = function (prefix) {
+  // 从根节点开始判断单词是否存在
+  let node = this.root;
+
+  // 在字典树中查找单词的每个字母
+  for (const char of prefix) {
+    // 如果当前字母不存在于字典树，该单词也一定不存在
+    if (!node[char]) {
+      return false;
+    }
+
+    // 移动节点，向下查找
+    node = node[char];
+  }
+
+  // 此处无需判断单词是否完整的在字典树中存在，只要字典树中存在单词所有字母即可
+  return true;
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * var obj = new Trie()
+ * obj.insert(word)
+ * var param_2 = obj.search(word)
+ * var param_3 = obj.startsWith(prefix)
+ */
+// @lc code=end
