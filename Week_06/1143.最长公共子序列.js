57b4c874f0fddfab8b87b3ae1db82ebcb12b7f7d
chencl1986 <chencl1986@qq.com>
2021-02-18 17:40:10 +0800

    提交作业

--- Week_06/1143.最长公共子序列.js
+++ Week_06/1143.最长公共子序列.js
@@ -2,6 +2,58 @@
  * @lc app=leetcode.cn id=1143 lang=javascript
  *
  * [1143] 最长公共子序列
+ *
+ * https://leetcode-cn.com/problems/longest-common-subsequence/description/
+ *
+ * algorithms
+ * Medium (60.69%)
+ * Likes:    326
+ * Dislikes: 0
+ * Total Accepted:    61.7K
+ * Total Submissions: 101.7K
+ * Testcase Example:  '"abcde"\n"ace"'
+ *
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长公共子序列的长度。
+ *
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde"
+ * 的子序列。两个字符串的「公共子序列」是这两个字符串所共同拥有的子序列。
+ *
+ * 若这两个字符串没有公共子序列，则返回 0。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ * 输入：text1 = "abcde", text2 = "ace"
+ * 输出：3
+ * 解释：最长公共子序列是 "ace"，它的长度为 3。
+ *
+ *
+ * 示例 2:
+ *
+ * 输入：text1 = "abc", text2 = "abc"
+ * 输出：3
+ * 解释：最长公共子序列是 "abc"，它的长度为 3。
+ *
+ *
+ * 示例 3:
+ *
+ * 输入：text1 = "abc", text2 = "def"
+ * 输出：0
+ * 解释：两个字符串没有公共子序列，返回 0。
+ *
+ *
+ *
+ *
+ * 提示:
+ *
+ *
+ * 1 <= text1.length <= 1000
+ * 1 <= text2.length <= 1000
+ * 输入的字符串只含有小写英文字符。
+ *
+ *
  */
 
 // @lc code=start
@@ -11,22 +63,32 @@
  * @return {number}
  */
 var longestCommonSubsequence = function (text1, text2) {
+  // 分别缓存两个字符串的长度
   const m = text1.length;
   const n = text2.length;
-  let dp = new Array(new Array(n + 1).fill(0));
+  // 初始化DP数组，默认值为第一行，对应text1为空字符串的情况，初始值都为0
+  let dp = [new Array(n + 1).fill(0)];
 
+  // 遍历text1
   for (let i = 1; i <= m; i++) {
+    // 存入当前行的状态，第一列对应了text2为空字符串的情况，初始值都为0
     dp.push(new Array(n + 1).fill(0));
 
+    // 遍历text2
     for (let j = 1; j <= n; j++) {
+      // 如果当前字母相等，当前位置一定可以加1，那么只需从text1[0]到text1[i-2]和text2[0]到text2[j-2]的公共子序列推导到此处即可
       if (text1[i - 1] === text2[j - 1]) {
-        dp[i][j] = 1 + dp[i - 1][j - 1];
+        dp[i][j] = dp[i - 1][j - 1] + 1;
       } else {
+        // 当前字母不相等
+        // 但从text1[0]到text1[i - 1]与text2[0]到text2[j - 2]，或者从text1[0]到text1[i - 2]与text2[0]到text2[j - 1]进行对比的话，都可能会出现公共子序列
+        // 由于涉及到两种情况，此处需要取最大值
         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
       }
     }
   }
 
+  // 推导完两个字符串长度，最后得到的就是最长公共子序列
   return dp[m][n];
 };
 // @lc code=end
--- Week_06/63. 不同路径 II，动态规划，二维数组.js
+++ Week_06/63. 不同路径 II，动态规划，二维数组.js
@@ -2,6 +2,59 @@
  * @lc app=leetcode.cn id=63 lang=javascript
  *
  * [63] 不同路径 II
+ *
+ * https://leetcode-cn.com/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (37.62%)
+ * Likes:    490
+ * Dislikes: 0
+ * Total Accepted:    122.2K
+ * Total Submissions: 325K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
+ *
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
+ *
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ *
+ *
+ *
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：2
+ * 解释：
+ * 3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：obstacleGrid = [[0,1],[0,0]]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1
+ * obstacleGrid[i][j] 为 0 或 1
+ *
+ *
  */
 
 // @lc code=start
@@ -10,39 +63,59 @@
  * @return {number}
  */
 var uniquePathsWithObstacles = function (obstacleGrid) {
-  const m = obstacleGrid.length;
-  const n = obstacleGrid[0].length;
+  // 缓存网格行列的最后一位索引
+  const m = obstacleGrid.length - 1;
+  const n = obstacleGrid[0].length - 1;
 
-  obstacleGrid[0][0] === 0
-    ? (obstacleGrid[0][0] = 1)
-    : (obstacleGrid[0][0] = 0);
+  // 如果起点或终点是1，必然无法走到终点，路径数量为0
+  if (obstacleGrid[0][0] === 1 || obstacleGrid[m][n] === 1) {
+    return 0;
+  }
 
-  for (let i = 1; i < n; i++) {
-    if (obstacleGrid[0][i] === 1) {
-      obstacleGrid[0][i] = 0;
+  // 初始化第一行的dp数组，起点的值为1
+  let dp = [new Array(n + 1).fill(0)];
+  dp[0][0] = 1;
+
+  // 创建第一列的初始路径数量
+  for (let i = 1; i <= m; i++) {
+    // 初始化当前行的初始路径数量，默认为0
+    dp.push(new Array(n + 1).fill(0));
+
+    if (obstacleGrid[i][0] === 0) {
+      // 如果当前位置可以行走，数量等于上一行
+      dp[i][0] = dp[i - 1][0];
     } else {
-      obstacleGrid[0][i] = obstacleGrid[0][i - 1];
+      // 如果当前位置不可行走，数量为0
+      dp[i][0] = 0;
     }
   }
 
-  for (let i = 1; i < m; i++) {
-    if (obstacleGrid[i][0] === 1) {
-      obstacleGrid[i][0] = 0;
+  // 初始化第一行的路径数量
+  for (let i = 0; i <= n; i++) {
+    if (obstacleGrid[0][i] === 0) {
+      // 如果当前位置可以行走，数量为1
+      dp[0][i] = 1;
     } else {
-      obstacleGrid[i][0] = obstacleGrid[i - 1][0];
+      // 如果当前位置不可行走，从这以后的数量都为0
+      // 第一行初始化的值都为0，直接退出即可
+      break;
     }
   }
 
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
+  // 从第二行、第二列开始遍历整个网格，计算路径数量
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
       if (obstacleGrid[i][j] === 0) {
-        obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+        // 如果当前位置可以行走，路径数量为从上、左两个方向走过来的数量之和
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
       } else {
-        obstacleGrid[i][j] = 0;
+        // 如果当前位置不可行走，路径数量为0
+        dp[i][j] = 0;
       }
     }
   }
 
-  return obstacleGrid[m - 1][n - 1];
+  // 终点存储的是所有路径数量
+  return dp[m][n];
 };
 // @lc code=end
